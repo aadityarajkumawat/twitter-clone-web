029636dbcc372f9ebecafe381b6ab3fd3c29c1a3
Aaditya Raj Kumawat <arkumawat78@gmail.com>
2021-01-16 18:53:39 +0530

    cleaned up component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import {
   useGetTweetsByUserQuery,
   useListenTweetsSubscription,
   useGetPaginatedPostsQuery,
+  GetPaginatedPostsQuery,
 } from "../generated/graphql";
 import * as S from "./home.styles";
 import { tweetAlreadyExist } from "../helpers/tweetAlreadyExist";
@@ -39,80 +40,6 @@ interface Scroller {
 const Home: React.FC<HomeProps> = () => {
   const [, postTweet] = useCreateTweetMutation();
   const [tweetInput, setTweetInput] = useState<string>("");
-  // First fetched tweets
-  const [
-    { data: tweets, fetching: fetchingTweets },
-  ] = useGetTweetsByUserQuery();
-  // Realtime added tweets state
-  const [realTimeAdded, setRealTimeAdded] = useState<Array<TweetItem>>([]);
-  // More loaded tweets state
-  const [morePosts, setMorePosts] = useState<Array<TweetItem>>([]);
-  const [paginationParams, setPaginationParams] = useState<PaginationParams>({
-    limit: -1,
-    offset: 0,
-  });
-  // Fetching realtime tweets
-  const [{ data: realtimePosts }] = useListenTweetsSubscription();
-  // Fetching more tweets
-  const [
-    { data: loadedPosts, fetching: fetchingMorePosts },
-  ] = useGetPaginatedPostsQuery({ variables: paginationParams });
-
-  const [scroller, setScroller] = useState<Scroller>({
-    dataLength: 2,
-    hasMore: true,
-  });
-
-  const loadMorePosts = (a: number, b: number, c: number) => {
-    let currentNumberOfPosts = a + b + c;
-    setPaginationParams((prev) => ({
-      ...prev,
-      limit: 2,
-      offset: currentNumberOfPosts,
-    }));
-  };
-
-  // console.log(loadedPosts);
-
-  useEffect(() => {
-    if (realtimePosts?.listenTweets.tweet?.tweet_id) {
-      const newTweet = realtimePosts.listenTweets.tweet;
-      const idOfThisTweet = newTweet.tweet_id;
-      let found = tweetAlreadyExist(
-        tweets!.getTweetsByUser.tweets,
-        realTimeAdded,
-        morePosts,
-        idOfThisTweet
-      );
-      if (!found) {
-        setRealTimeAdded((prev) => [newTweet, ...prev]);
-      }
-    }
-    //eslint-disable-next-line
-  }, [realtimePosts?.listenTweets.tweet?.tweet_id]);
-
-  useEffect(() => {
-    if (loadedPosts) {
-      let moreP = loadedPosts.getPaginatedPosts.tweets;
-      moreP = moreP.slice(moreP.length - 2, moreP.length);
-      setMorePosts((prev) => [...prev, ...moreP]);
-    }
-    //eslint-disable-next-line
-  }, [JSON.stringify(loadedPosts?.getPaginatedPosts.tweets)]);
-
-  const g = () => {
-    let a = realTimeAdded.length;
-    let b = 7;
-    let c = morePosts.length;
-
-    if (paginationParams.offset === tweets?.getTweetsByUser.num) {
-      setScroller((prev) => ({ ...prev, hasMore: false }));
-    }
-
-    setScroller((prev) => ({ ...prev, dataLength: prev.dataLength + 2 }));
-
-    loadMorePosts(a, b, c);
-  };
 
   return (
     <S.BaseComponent>
@@ -142,62 +69,7 @@ const Home: React.FC<HomeProps> = () => {
           </S.CreateTweet>
         </S.FeedHeader>
 
-        <S.Tweets>
-          {realTimeAdded.length > 0 && (
-            <Fragment>
-              {realTimeAdded.map((tweet) => (
-                <Tweet
-                  name={tweet.name}
-                  tweet_content={tweet.tweet_content}
-                  key={tweet.tweet_id}
-                  username={tweet.username}
-                  comments={tweet.comments}
-                  tweet_id={tweet.tweet_id}
-                />
-              ))}
-            </Fragment>
-          )}
-          {!fetchingTweets ? (
-            <Fragment>
-              {tweets!.getTweetsByUser.tweets.map((tweet) => (
-                <Tweet
-                  name={tweet.name}
-                  tweet_content={tweet.tweet_content}
-                  key={tweet.tweet_id}
-                  username={tweet.username}
-                  comments={tweet.comments}
-                  tweet_id={tweet.tweet_id}
-                />
-              ))}
-            </Fragment>
-          ) : (
-            <Fragment>Loading...</Fragment>
-          )}
-
-          <InfiniteScroll
-            dataLength={scroller.dataLength}
-            next={g}
-            hasMore={scroller.hasMore}
-            loader={<h4>Loading...</h4>}
-          >
-            {!fetchingMorePosts ? (
-              <Fragment>
-                {morePosts.map((tweet) => (
-                  <Tweet
-                    name={tweet.name}
-                    tweet_content={tweet.tweet_content}
-                    key={tweet.tweet_id}
-                    username={tweet.username}
-                    comments={tweet.comments}
-                    tweet_id={tweet.tweet_id}
-                  />
-                ))}
-              </Fragment>
-            ) : (
-              <Fragment>Fetching more posts...</Fragment>
-            )}
-          </InfiniteScroll>
-        </S.Tweets>
+        <S.Tweets></S.Tweets>
       </S.HomeMain>
     </S.BaseComponent>
   );
