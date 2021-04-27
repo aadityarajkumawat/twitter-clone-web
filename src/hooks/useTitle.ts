f4410d37d286061961fa3c2ebfccff66bb648c80
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-04-28 00:03:56 +0530

    added title hook

--- src/hooks/useTitle.ts
+++ src/hooks/useTitle.ts
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
+import { useLocation } from "react-router";
 
 export const useTitle = (title: string) => {
+  const location = useLocation();
   useEffect(() => {
+    console.log("cool");
     document.title = title;
-  }, []);
+  }, [location]);
 };
